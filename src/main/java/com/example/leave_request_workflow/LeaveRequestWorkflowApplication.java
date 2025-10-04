08064b55dc0fb73c91fd361f47e0a6bd16eb1350
yyoshidaweb <yoshidayumajob@gmail.com>
2025-10-04 09:24:23 +0900

    first commit

---
+++ src/main/java/com/example/leave_request_workflow/LeaveRequestWorkflowApplication.java
@@ -0,0 +1,13 @@
+package com.example.leave_request_workflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeaveRequestWorkflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeaveRequestWorkflowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/leave_request_workflow/LeaveRequestWorkflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.leave_request_workflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeaveRequestWorkflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
